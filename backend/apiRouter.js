4601999f5a36f21dd9dda46c4a81e22c676903ce
antoine <a0415@hotmail.fr>
2022-03-22 14:50:50 +0100

    test rapide

--- backend/apiRouter.js
+++ backend/apiRouter.js
@@ -8,8 +8,9 @@ exports.router = (function() {
     const apiRouter = express.Router();
 
     //User routes
-    apiRouter.route('/users/signup/').post(userCtrl.signup),
+    apiRouter.route('/users/signup/').post(userCtrl.signup);
     apiRouter.route('/users/login/').post(userCtrl.login);
+    apiRouter.route('/users/me/').get(userCtrl.getUserProfile);
 
     return apiRouter;
 })();
--- backend/routes/userCtrl.js
+++ backend/routes/userCtrl.js
@@ -1,6 +1,11 @@
 const bcrypt = require('bcrypt');
-const models = require('../models')
-const jwtUtils = require('../utils/jwt.utils')
+const models = require('../models');
+const jwtUtils = require('../utils/jwt.utils');
+const asyncLib = require('async');
+const { verify } = require('jsonwebtoken');
+
+//regex
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
 
 //routes
 
@@ -13,69 +18,191 @@ module.exports = {
             return res.status(400).json({ 'error': 'missing parameters' });
         }
 
-        models.User.findOne({
-            attributes: ['userName'],
-            where: { userName: userName }
-        })
+        if (userName.length >= 13 || userName.length <= 4 ){
+            return res.status(400).json({ 'error': 'wrong username (must be length 5 - 12'});
+        }
+
+        if (!PASSWORD_REGEX.test(password)) {
+            return res.status(400).json({ 'error': 'password invalid(must be length 4-8 include 1 number at least'})
+        }
+
+        asyncLib.waterfall([
+            function(done) {
+                models.User.findOne({
+                    attributes: ['userName'],
+                    where: { userName: userName }
+                })
             .then(function (userFound) {
-                if (!userFound) {
-
-                    bcrypt.hash(password, 5, function (err, bcryptedPassword) {
-                        const newUser = models.User.create({
-                            userName: userName,
-                            password: bcryptedPassword,
-                            isAdmin: 0
-                        })
-                            .then(function (newUser) {
-                                return res.status(201).json({
-                                    'userId': newUser.id
-                                })
-                            })
-                            .catch(function (err) {
-                                return res.status(500).json({ 'error': 'cannot add user' });
-                            });
-
-                    });
-                } else {
-                    return res.status(409).json({ 'error': 'user already existe' })
-                }
-
+                    done(null, userFound);
             })
-            .catch(function (err) {
-                return res.status(500).json({ 'error': 'unable to verify user' })
+            .catch(function(err) {
+                return res.status(500).json({'error': 'unable to verify user'});
             });
+            },
+            function(userFound, done) {
+                if(!userFound) {
+                    bcrypt.hash(password, 5, function(err,bcryptedPassword){
+                        done(null, userFound, bcryptedPassword);
+                    })
+                } else {
+                    return res.stauts(409).json({ 'error' : 'user already exist' });
+                }
+            },
+            function(userFound, bcryptedPassword, done) {
+                const newUser = models.User.create({
+                    userName: userName,
+                    password: bcryptedPassword,
+                    isAdmin: 0
+                })
+                .then(function(newUser) {
+                    done(newUser);
+                })
+                .catch(function(err){
+                    return res.status(500).json({ 'error' : 'cannot add user'});
+                });
+            }
+        ], function(newUser) {
+            if (newUser) {
+                return res.status(200).json({
+                    'userId': newUser.id
+                });
+            } else {
+                return res.status(404).json({ 'error': 'cannot add user'});
+            }
+        });
+
+    //     models.User.findOne({
+    //         attributes: ['userName'],
+    //         where: { userName: userName }
+    //     })
+    //         .then(function (userFound) {
+    //             if (!userFound) {
+
+    //                 bcrypt.hash(password, 5, function (err, bcryptedPassword) {
+    //                     const newUser = models.User.create({
+    //                         userName: userName,
+    //                         password: bcryptedPassword,
+    //                         isAdmin: 0
+    //                     })
+    //                         .then(function (newUser) {
+    //                             return res.status(201).json({
+    //                                 'userId': newUser.id
+    //                             })
+    //                         })
+    //                         .catch(function (err) {
+    //                             return res.status(500).json({ 'error': 'cannot add user' });
+    //                         });
+
+    //                 });
+    //             } else {
+    //                 return res.status(409).json({ 'error': 'user already existe' })
+    //             }
+
+    //         })
+    //         .catch(function (err) {
+    //             return res.status(500).json({ 'error': 'unable to verify user' })
+    //         });
 
     },
-    login: function (req, res) {
-        //params
-        const userName = req.body.userName;
-        const password = req.body.password;
-        if (userName == null || password == null) {
-            return res.status(400).json({ 'error': 'missing parameters' });
-        }
+//     login: function (req, res) {
+//         //params
+//         const userName = req.body.userName;
+//         const password = req.body.password;
+//         if (userName == null || password == null) {
+//             return res.status(400).json({ 'error': 'missing parameters' });
+//         }
+//         models.User.findOne({
+//             where: { userName: userName }
+//         })
+//         .then(function(userFound){
+//             if(userFound){
+//                 bcrypt.compare(password, userFound.password, function(errBycrypt, resbcrypt){
+//                     if(resbcrypt) {
+//                         return res.status(200).json({
+//                             'userId': userFound.id,
+//                             'token': jwtUtils.generateTokenForUser(userFound)
+//                         });
+//                     }else{
+//                         return res.status(403).json({ "error": "invalid password"});
+//                     }
+//                 })
+
+//             }else{
+//                 return res.status(404).json({ 'error': 'user not exist in DB'});
+//             }
+
+//         })
+//         .catch(function (err) {
+//             return res.status(500).json({ 'error': 'unable to verify user' })
+//         });
+//     }
+// };
+login: function(req, res) {
+    
+    // Params
+    var userName = req.body.userName;
+    var password = req.body.password;
+
+    if (userName == null ||  password == null) {
+      return res.status(400).json({ 'error': 'missing parameters' });
+    }
+    asyncLib.waterfall([
+      function(done) {
         models.User.findOne({
-            where: { userName: userName }
+          where: { userName: userName }
         })
-        .then(function(userFound){
-            if(userFound){
-                bcrypt.compare(password, userFound.password, function(errBycrypt, resbcrypt){
-                    if(resbcrypt) {
-                        return res.status(200).json({
-                            'userId': userFound.id,
-                            'token': jwtUtils.generateTokenForUser(userFound)
-                        });
-                    }else{
-                        return res.status(403).json({ "error": "invalid password"});
-                    }
-                })
+        .then(function(userFound) {
+          done(null, userFound);
+        })
+        .catch(function(err) {
+          return res.status(500).json({ 'error': 'unable to verify user' });
+        });
+      },
+      function(userFound, done) {
+        if (userFound) {
+          bcrypt.compare(password, userFound.password, function(errBycrypt, resBycrypt) {
+            done(null, userFound, resBycrypt);
+          });
+        } else {
+          return res.status(404).json({ 'error': 'user not exist in DB' });
+        }
+      },
+      function(userFound, resBycrypt, done) {
+        if(resBycrypt) {
+          done(userFound);
+        } else {
+          return res.status(403).json({ 'error': 'invalid password' });
+        }
+      }
+    ], function(userFound) {
+      if (userFound) {
+        return res.status(201).json({
+          'userId': userFound.id,
+          'token': jwtUtils.generateTokenForUser(userFound)
+        });
+      } else {
+        return res.status(500).json({ 'error': 'cannot log on user' });
+      }
+    });
+},
+    getUserProfile: function(req, res) {
+        const headerAuth = req.headers['authorization'];
+        const userId = jwtUtils.getUserId(headerAuth);
 
-            }else{
-                return res.status(404).json({ 'error': 'user not exist in DB'});
+        if (userId < 0)
+        return res.status(400).json({ 'error': 'wrong token'});
+
+        models.User.findOne({
+            attributes: ['id', 'userName'],
+            where: {id: userId}
+        }).then(function(user) {
+            if (user) {
+                res.status(201).json(user);
+            } else {
+                res.status(404).json({ 'error': 'user not found'});
             }
-
-        })
-        .catch(function (err) {
-            return res.status(500).json({ 'error': 'unable to verify user' })
+        }).catch(function(err){
+            res.status(500).json({ 'error': 'cannot fetch user'});
         });
     }
 };
